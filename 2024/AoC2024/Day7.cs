4341788f0664b20ca624eb563e534bb490fb0324
Moshe Steinberg <moshe.steinberg@aon.com>
2025-01-02 22:55:03 +0000

    Copilot inspired stylistic improvements to code :)

--- 2024/AoC2024/Day7.cs
+++ 2024/AoC2024/Day7.cs
@@ -7,28 +7,23 @@ public static class Day7
 
         string[] readFile = File.ReadAllLines(@"C:\Users\A0828886\01 Repositories\Advent of code\2024\AoC2024\Input\7.txt");
         
-        long countAnswer = 0;
         var formattedInput = readFile
                                 .Select(x => x.Split(": ", StringSplitOptions.None))
                                 .ToDictionary(x => Int64.Parse(x[0]), x => x[1].Split(' ').Select(y => Int64.Parse(y)).ToList());
 
-        foreach( var (key, listValue) in formattedInput)
-        {
-            if (AllCombos(listValue).Contains(key))
-            {
-                countAnswer += key;
-            };
+        long countAnswer = formattedInput
+            .Where( kv => AllCombos(kv.Value).Contains(kv.Key))
+            .Sum(kv => kv.Key);
 
-        }
         Console.WriteLine($"Answer: {countAnswer}");
         
     }
 
     public static List<long> AllCombos(List<long> listValues)
     {
-        int length = listValues.Count;
+
         List<long> vals = [listValues[0]];
-        for (int i = 1; i < length; i++)
+        for (int i = 1; i < listValues.Count; i++)
         {
             List<long> newVals = [];
             foreach (var val in vals)
