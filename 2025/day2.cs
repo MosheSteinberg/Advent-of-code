601b5c03bb251b115ecbfff88116d9acfc8ce209
Moshe Steinberg <moshe.steinberg@aon.com>
2025-12-02 21:49:32 +0200

    day2 part 2 working on sample

--- 2025/day2.cs
+++ 2025/day2.cs
@@ -1,13 +1,15 @@
-var lines = File.ReadAllLines("Input/2.txt");
+var lines = File.ReadAllLines("Input/2sample.txt");
 var contents = string.Join("", lines);
-var answer1 = contents
+var allIds = contents
     .Split(',')
     .Select(s => 
         {
             var parts = s.Split('-');
             return CreateRange(long.Parse(parts[0]), long.Parse(parts[1]));
         })
-    .SelectMany(x => x)
+    .SelectMany(x => x);
+
+var answer1 = allIds
     .Where(x =>
     {
         var str = x.ToString();
@@ -27,6 +29,16 @@ var answer1 = contents
 
 Console.WriteLine($"Answer 2a: {answer1}");
 
+var answer2 = allIds
+    .Where(x =>
+    {
+        var str = x.ToString();
+        return IsStringRepeated(x.ToString());
+    })
+    .Sum();
+
+Console.WriteLine($"Answer 2b: {answer2}");
+
 static IEnumerable<long> CreateRange(long start, long end)
 {
     while (start <= end)
@@ -34,4 +46,42 @@ static IEnumerable<long> CreateRange(long start, long end)
         yield return start;
         start++;
     }
+}
+
+static IEnumerable<(int, int)> GetDivisors(int number)
+{
+    for (int i = 1; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            yield return (i, number / i);
+        }
+    }
+}
+
+static bool IsStringRepeated(string str)
+{
+    var lenStr = str.Length;
+
+    foreach(var (x, y) in GetDivisors(lenStr))
+    {
+        for (int part = 1; part < y; part++)
+        {
+            if (str.Substring(0, x) != str.Substring(part * x, x))
+                break;
+
+            if (part == y - 1)
+                return true;
+        }
+
+        for (int part = 1; part < x; part++)
+        {
+            if (str.Substring(0, y) != str.Substring(part * y, y))
+                break;
+
+            if (part == x - 1)
+                return true;
+        }
+    }
+    return false;
 }
