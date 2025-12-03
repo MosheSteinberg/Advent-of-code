569ce95fde0389759c4c9927e000c05c4df306d8
Moshe Steinberg <moshe.steinberg@aon.com>
2025-12-03 09:48:52 +0200

    day 3 part 2 working on sample

--- 2025/day3.cs
+++ 2025/day3.cs
@@ -1,4 +1,4 @@
-var input = File.ReadLines("Input/3.txt");
+var input = File.ReadLines("Input/3sample.txt");
 
 var prep = input
     .Select(line => line.ToCharArray().Select(c => int.Parse(c.ToString())));
@@ -13,3 +13,22 @@ var answer1 = prep
     .Sum();
 
 Console.WriteLine($"Answer 3a: {answer1}");
+
+var answer2 = prep
+    .Select(row =>
+    {
+        string result = "";
+        for(int i = 0; i < 12; i++)
+        {
+            // max in all bar last 11 - i positions
+            // skipping based on location from previous iteration
+            var max = row.Take(row.Count() - (11 - i)).Max();
+            var locMax = row.ToList().IndexOf(max);
+            result += max.ToString();
+            row = row.Skip(locMax + 1);
+        }
+        return long.Parse(result);
+    })
+    .Sum();
+
+Console.WriteLine($"Answer 3b: {answer2}");
