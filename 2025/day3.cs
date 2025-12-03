4697dbfe05b6127195a6e6804943cbd74213d0ec
Moshe Steinberg <moshe.steinberg@aon.com>
2025-12-03 09:40:24 +0200

    Initial 3a with sample

---
+++ 2025/day3.cs
@@ -0,0 +1,15 @@
+var input = File.ReadLines("Input/3sample.txt");
+// max up to last number
+
+var answer1 = input
+    .Select(line => line.ToCharArray().Select(c => int.Parse(c.ToString())))
+    .Select(row => 
+    {
+        var max = row.Take(row.Count() - 1).Max();
+        var locMax = row.ToList().IndexOf(max);
+        var maxPostMax = row.Skip(locMax+1).Max();
+        return int.Parse(max.ToString() + maxPostMax.ToString());
+    })
+    .Sum();
+
+Console.WriteLine($"Answer 3a: {answer1}");
